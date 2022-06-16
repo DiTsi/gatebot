72797ff25d9a2fa3ab6442fe7d824a8302832a93
Dmitry Tsybus <tsibous14@gmail.com>
2022-06-16 18:39:06 +0300

    First installed version - actual group id - comment out button code - filter_time function

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import logging
 import requests
 import threading
 import json
-from time import sleep
+import time
 
 
 BOT_TOKEN = 'nopass'
-# GROUP = -629434310
-GROUP = -nogroup #! test group
+# GROUP = -nogroup
+# GROUP = -nogroup #! test group
+GROUP = -nogroup #! test group 2
 SILENT = True
-ENABLE_BUTTON_NOTIFICATION = True
+# ENABLE_BUTTON_NOTIFICATION = True
 
 
 class Bot:
@@ -47,11 +48,22 @@ def filter_mentions(updates):
 
 
 def filter_group(updates, group_id):
+    result = list()
     for u in updates:
         message = u['message']
-        if message['chat']['id'] != group_id:
-            updates.remove(u)
-    return updates
+        if message['chat']['id'] == group_id:
+            result.append(u)
+    return result
+
+
+def filter_time(updates, group_id):
+    result = list()
+    for u in updates:
+        message = u['message']
+        logging.info('timediff = %s' % (time.time() - int(message['date'])))
+        if time.time() - int(message['date']) < 30 :
+            result.append(u)
+    return result
 
 
 def get_last_id(updates):
@@ -65,55 +77,56 @@ def get_messages(updates):
     return messages
 
 
-def enable_notification():
-    global ENABLE_BUTTON_NOTIFICATION
+# def enable_notification():
+#     global ENABLE_BUTTON_NOTIFICATION
     
-    ENABLE_BUTTON_NOTIFICATION = True
+#     ENABLE_BUTTON_NOTIFICATION = True
 
 
-def button_handler(userdata):
-    global ENABLE_BUTTON_NOTIFICATION
+# def button_handler(userdata):
+#     global ENABLE_BUTTON_NOTIFICATION
 
-    if ENABLE_BUTTON_NOTIFICATION:
-        ENABLE_BUTTON_NOTIFICATION = False
-        logging.info('button pressed')
-        bot.send_message(GROUP, 'Button pressed, please open the gate')
-        timer.start()
+#     if ENABLE_BUTTON_NOTIFICATION:
+#         ENABLE_BUTTON_NOTIFICATION = False
+#         logging.info('button pressed')
+#         bot.send_message(GROUP, 'Button pressed, please open the gate')
+#         timer.start()
 
 
 
 bot = Bot(BOT_TOKEN)
-
-
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level='INFO')
-timer = threading.Timer(60.0, enable_notification)
+# timer = threading.Timer(60.0, enable_notification)
 
 # gate output
 gate = mraa.Gpio(2)
 gate.dir(mraa.DIR_OUT)
 
 # button
-pin = mraa.Gpio(3)
-pin.dir(mraa.DIR_IN)
-pin.isr(mraa.EDGE_FALLING, button_handler, None)
+# pin = mraa.Gpio(3)
+# pin.dir(mraa.DIR_IN)
+# pin.isr(mraa.EDGE_FALLING, button_handler, None)
 
 offset = None
 while True:
     logging.info('getting messages')
 
     updates = bot.get_updates(offset=offset)
-    updates = filter_mentions(updates)
-    updates = filter_group(updates, GROUP)
+    
+    filtered_updates = filter_mentions(updates)
+    filtered_updates = filter_group(filtered_updates, GROUP)
+    filtered_updates = filter_time(filtered_updates, GROUP)
+    
     updates, last_id = get_last_id(updates)
     offset = last_id + 1 if last_id is not None else None
 
-    messages = get_messages(updates)
+    messages = get_messages(filtered_updates)
     if len(messages):
         gate.write(1)
-        sleep(0.5)
+        time.sleep(0.25)
         gate.write(0)
 
         if not SILENT: bot.send_message(GROUP, 'done')
         logging.info('open / close gate')
 
-    sleep(3)
+    time.sleep(3)
