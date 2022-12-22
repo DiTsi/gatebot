293e468f78e96a8a7a737313238550b1e06f905a
Dmitry Tsybus <tsibous14@gmail.com>
2022-12-22 23:18:52 +0300

    python3 ang orangepi migration

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
-import mraa
+import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import requests
-import threading
 import json
 import time
 
 
-BOT_TOKEN = 'nopass'
-GROUP = -nogroup #! "Parkovka. Bot" group
-# GROUP = -nogroup #! "Parkovka" main group
-# GROUP = -nogroup #! test group
-# GROUP = -nogroup #! test group 2
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+GROUP = os.getenv('BOT_GROUP')
 SILENT = True
-MESSAGE_TIMEOUT = 30 # seconds
+MESSAGE_TIMEOUT_SECONDS = 30
 # ENABLE_BUTTON_NOTIFICATION = True
 
 
@@ -42,36 +38,47 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
+class Gate:
+    def __init__(self, pinout):
+        self.pinout = str(pinout)
+        os.system(f'sudo echo {self.pinout} > /sys/class/gpio/export')
+        os.system(f"sudo echo out > /sys/class/gpio/gpio{self.pinout}/direction")
+
+    def open(self):
+        os.system(f"echo 1 > /sys/class/gpio/gpio{self.pinout}/value")
+
+    def close(self):
+        os.system(f"echo 0 > /sys/class/gpio/gpio{self.pinout}/value")
+
+
 class Bot:
     def __init__(self, token):
         self.token = token
         self.link = "https://api.telegram.org/bot" + self.token
 
-
-    def get_updates(self, offset=None):
+    def get_updates(self, offset_=None):
         try:
-            r = requests.get(self.link + "/getUpdates", params={"offset": offset})
+            r = requests.get(self.link + "/getUpdates", params={"offset": offset_})
             content = json.loads(r.content.decode())
             return content['result']
-        except:
+        except Exception:
             logger.warning('Can\'t send GET request')
             return {}
 
-
     def send_message(self, chat_id, message):
         try:
-            r = requests.get(self.link + "/sendMessage", params={
+            requests.get(self.link + '/sendMessage', params={
                 'chat_id': chat_id,
                 'text': message,
                 })
-        except:
+        except Exception:
             logger.warning('Can\'t send message')
 
 
-def filter_mentions(updates):
+def filter_mentions(updates_):
     result = list()
 
-    for u in updates:
+    for u in updates_:
         message = u['message']
         if 'entities' in message.keys():
             if message['entities'][0]['type'] == 'mention':
@@ -79,35 +86,35 @@ def filter_mentions(updates):
     return result
 
 
-def filter_group(updates, group_id):
+def filter_group(updates_, group_id):
     result = list()
 
-    for u in updates:
+    for u in updates_:
         message = u['message']
         if message['chat']['id'] == group_id:
             result.append(u)
     return result
 
 
-def filter_time(updates):
+def filter_time(updates_):
     result = list()
     
-    for u in updates:
+    for u in updates_:
         message = u['message']
-        if time.time() - int(message['date']) < MESSAGE_TIMEOUT:
+        if time.time() - int(message['date']) < MESSAGE_TIMEOUT_SECONDS:
             result.append(u)
     return result
 
 
-def get_last_id(updates):
-    return updates, updates[-1]['update_id'] if len(updates) else None
+def get_last_id(updates_):
+    return updates_, updates_[-1]['update_id'] if len(updates_) else None
 
 
-def get_messages(updates):
-    messages = []
-    for u in updates:
-        messages.append(u['message'])
-    return messages
+def get_messages(updates_):
+    messages_ = list()
+    for u in updates_:
+        messages_.append(u['message'])
+    return messages_
 
 
 # def enable_notification():
@@ -128,27 +135,24 @@ def get_messages(updates):
 bot = Bot(BOT_TOKEN)
 
 logger = logging.getLogger('main_logger')
-logger.setLevel('INFO')
-rfh = TimedRotatingFileHandler('/opt/telegram_bot/logs/bot.log', when='midnight', backupCount=7) # 1.7Mb per day with 'INFO' level
+logger.setLevel('INFO')  # 1.7Mb per day with 'INFO' level
+rfh = TimedRotatingFileHandler('/opt/telegram_bot/logs/bot.log', when='midnight', backupCount=7)
 rfh.setFormatter(CustomFormatter())
 logger.addHandler(rfh)
-
+gate = Gate(227)
 # timer = threading.Timer(60.0, enable_notification)
 
-# gate output
-gate = mraa.Gpio(2)
-gate.dir(mraa.DIR_OUT)
 
 # button
-# pin = mraa.Gpio(3)
-# pin.dir(mraa.DIR_IN)
-# pin.isr(mraa.EDGE_FALLING, button_handler, None)
+# pin = mraa_.Gpio(3)
+# pin.dir(mraa_.DIR_IN)
+# pin.isr(mraa_.EDGE_FALLING, button_handler, None)
 
 offset = None
 while True:
     logger.debug('getting messages')
 
-    updates = bot.get_updates(offset=offset)
+    updates = bot.get_updates(offset_=offset)
     
     filtered_updates = filter_mentions(updates)
     filtered_updates = filter_group(filtered_updates, GROUP)
@@ -159,12 +163,12 @@ while True:
 
     messages = get_messages(filtered_updates)
     if len(messages):
-        gate.write(1)
+        gate.open()
         time.sleep(0.25)
-        gate.write(0)
+        gate.close()
 
-        if not SILENT: bot.send_message(GROUP, 'done')
+        if not SILENT:
+            bot.send_message(GROUP, 'done')
         logger.info('open / close gate')
 
     time.sleep(3)
-
